97216806db8c432c6c70ac03dd3abfed8d1b622e
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-10-26 16:40:37 +0300

    basic html

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,29 @@ function App() {
 
   return (
     <div>
-      Hello
+      <form>
+        <h1>Login</h1>
+        <div className="input-box">
+          <input type="text" placeholder='Username' required/>
+          <i className='bx bxs-user'></i>
+        </div>
+        <div className="input-box">
+          <input type="password" placeholder='Password' required/>
+          <i className='bx bxs-lock-alt'></i>
+        </div>
+
+        <div className="remember-forgot">
+          <label><input type='checkbox'/>Remember me</label>
+          <a href='#'>Forgot password?</a>
+        </div>
+
+        <button type='submit'>Login</button>
+
+        <div className="register-link">
+          <p>Don't have an account? <a href='#'>Register</a></p>
+        </div>
+        
+      </form>
     </div>
   )
 }
