769fe3973559f59fd87cfce6649fbd4edbfcabc2
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-10-27 10:50:16 +0300

    add border opacity

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,18 @@ import './App.css'
 function App() {
 
   return (
-    <div className='w-[420px] text-white bg-transparent py-[30px] px-[40px] rounded-[10px] border-solid border-2 border-white backdrop-blur-[30px] shadow-sm'>
+    <div className='w-[420px] text-white bg-transparent py-[30px] px-[40px] rounded-[10px] border-solid border-2 border-white backdrop-blur-[30px] shadow-sm border-opacity-20'>
       <form>
         <h1 className='text-3xl text-center'>Login</h1>
         <div className="w-full h-[50px] mx-0 my-[30px] relative">
           <input 
-            className='placeholder-white w-full h-full bg-transparent outline-none border-2 border-solid border-white rounded-[40px] text-base py-5 pr-[45px] pl-5'
+            className='placeholder-white w-full h-full bg-transparent outline-none border-2 border-solid border-white rounded-[40px] border-opacity-20 text-base py-5 pr-[45px] pl-5'
             type="text" placeholder='Username' required/>
           <i className='bx bxs-user absolute right-5 top-2/4 -translate-y-1/2 text-xl'></i>
         </div>
         <div className="w-full h-[50px] mx-0 my-[30px] relative">
           <input 
-            className='placeholder-white w-full h-full bg-transparent outline-none border-2 border-solid border-white rounded-[40px] text-base py-5 pr-[45px] pl-5'
+            className='placeholder-white w-full h-full bg-transparent outline-none border-2 border-solid border-white rounded-[40px] border-opacity-20 text-base py-5 pr-[45px] pl-5'
             type="password" placeholder='Password' required/>
           <i className='bx bxs-lock-alt absolute right-5 top-2/4 -translate-y-1/2 text-xl'></i>
         </div>
